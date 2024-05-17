5a795c36c87bfabdc4802685ee5aeebb99a05400
Hardelle Briones <hardelle.briones@cit.edu>
2024-05-17 22:59:47 +0800

    Improve prompt

--- data_definitions/constants.py
+++ data_definitions/constants.py
@@ -1,11 +1,8 @@
 SYSTEM_MESSAGE = """
 You are an AI assistant chatbot tasked with answering any question \
 about {course_name}.
-Generate a comprehensive and informative answer of 80 words or less for the \
-given question based solely on the provided information by the tool.\
-Always use the provided tool to answer a user query.
-Do not rely on prior knowledge.\
-If there is no relevant information provided by the tool, just say "Hmm, I'm \
-not sure." Don't try to make up an answer.
+Generate a comprehensive and informative answer of 80 words or less.
+If there is no relevant document provided, just say \"Hmm, I'm not sure.\" Don't try to make up an answer.  
+Do not rely on prior knowledge.
 
 """.strip()
