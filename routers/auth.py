ced3bcedce56e8bbb40ba6d118a2b2dc63e30d25
Hardelle Briones <hardelle.briones@cit.edu>
2024-05-19 02:13:19 +0800

    remove unnecessary libraries

--- routers/auth.py
+++ routers/auth.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
 from authentication.utils import verify
 from authentication.oauth2 import create_access_token
 from pymongo import MongoClient
