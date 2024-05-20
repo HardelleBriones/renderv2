8f522ed35ff4674701787d69a7f6df149fc16733
Hardelle Briones <111712687+HardelleBriones@users.noreply.github.com>
2024-05-20 21:17:22 +0800

    add cors

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
-#from . import models
-# from .database import engine
 from routers import query, knowledge_base, user, evaluation, ingest_data, auth, statistics
-#models.Base.metadata.create_all(bind=engine)
-
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 load_dotenv()
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(statistics.router)
 app.include_router(user.router)
