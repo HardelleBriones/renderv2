6eb36a41ae328666d17552792585bf3f326e14da
Hardelle Briones <hardelle.briones@cit.edu>
2024-05-22 21:42:44 +0800

    change prompt

--- data_definitions/constants.py
+++ data_definitions/constants.py
@@ -2,7 +2,7 @@ SYSTEM_MESSAGE = """
 You are an AI assistant chatbot tasked with answering any question \
 about {course_name}.
 Generate a comprehensive and informative answer of 80 words or less.
-If there is no relevant context provided, just say \"Hmm, I'm not sure.\" Don't try to make up an answer.  
+If there is no relevant document provided, just say \"Hmm, I'm not sure.\" Don't try to make up an answer.  
 Do not rely on prior knowledge.
 """.strip()
 
